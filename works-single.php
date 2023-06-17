8312cbda59e87745ac0981fac8775a3a2108925f
Kimy DAVID <kimy.david@calliweb.fr>
2023-06-17 16:24:40 +0200

    refonte page work single

--- works-single.php
+++ works-single.php
@@ -1,5 +1,4 @@
 <?php 
-
 	if(have_posts()) { 
 		the_post(); 
 	}elseif(wp_verify_nonce( $_REQUEST['token'], "wp_token" )) { 
@@ -10,172 +9,129 @@
 	// AJAX TOKEN
 	$token = wp_create_nonce("wp_token");
 	
-	
 	$prev_post = getNextBack('prev','works',$post->menu_order,$post->post_date);
 	$next_post = getNextBack('next','works',$post->menu_order,$post->post_date);
 
-		?>	
-        
-<div id="singlecontent">
-
-		<div class="columns navibg border-color" style="display:none;">
-            <div class="four columns alpha">
-            	<h3><?php _e('Works','dronetv'); ?></h3>
-            </div>
-            
-            <div class="sixteen columns omega">
-            	<div class="navigate">
-                	<hr class="resshow border-color" /> 
-                    <span class="pname"></span>
-                    <a href="<?php echo get_home_url(); ?>" data-title="All" title="<?php echo __("All Projects","dronetv");?>" data-type="works" data-token="<?php echo $token?>" class="navigate parent getworks-showmsg gohome">&nbsp;</a>
-                    <?php if(!empty( $prev_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($prev_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $prev_post['ID']?>" title="<?php echo htmlspecialchars($prev_post['post_title'])?>" class="navigate back getworks-nextback getworks-showmsg">&nbsp;</a>
-                    <?php } ?>
-                    <?php if(!empty( $next_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($next_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $next_post['ID']?>" title="<?php echo htmlspecialchars($next_post['post_title'])?>" class="navigate next getworks-nextback getworks-showmsg">&nbsp;</a>
-					<?php } ?>
-            	</div>
-            </div>	
-        </div>
-
-         <div class="columns navibg withall border-color sisi">
-            <div class="four columns alpha">
-            	<h3><?php _e('Works','dronetv'); ?></h3>
-            </div>
-            
-            <div class="sixteen columns omega" style="margin-left:0;">
-            	<div id="portfolio-cats" class="navigate">
-                	<hr class="resshow border-color" />
-                    <div class="fullnav">
-					<?php 
-                        // GET CATEGORIES
-						$tp = 'works-categories';
-                        $cats = get_terms( $tp ); 
-                        $count_cats = count( $cats ); 
-                        if ( $count_cats > 0 ) {
-                    ?>
-                        <a href="/" data-rel="all" class="activemenu-bg <?php if(!isset($term)) { echo 'selected'; } ?>" data-th="<?php echo $getbr[3]?>" title="<?php echo __("ALL","dronetv");?>"><?php echo __("ALL","dronetv"); ?></a>
-                    <?php		
-                       foreach ($cats as $catd) { 
-                    ?>
-                        <a href="<?php echo esc_attr(get_term_link( $catd, $tp )); ?>" class="activemenu-bg <?php if(isset($term) && $catd->slug==$term) { echo 'selected'; } ?>" data-th="<?php echo $getbr[3]?>" data-rel="<?php echo $catd->slug; ?>" title="<?php if(isset($term)) echo $term->name; ?>"><?php echo strtoupper($catd->name); ?></a>
-                    <?php } ?>
-                    <?php	} ?>
-                    </div>
-                    <select class="responsiveselect reschange border-color">
-                    	<option value="all" selected=""><?php _e('Creative Fields...','dronetv')?></option>
-                        <?php		
-                       foreach ($cats as $catd) { 
-                    	?>
-                        <option value="<?php echo $catd->slug; ?>"><?php echo $catd->name; ?></option>
-                    	<?php } ?>
-                    </select>
-            	</div>
-            </div>	
-        </div>
-
-	<?php
+    $permalink = get_permalink( $post->ID );
+    $work_media = unserialize(get_post_meta( $post->ID, 'work-media', true ));
+    $work_client = get_post_meta( $post->ID, 'work-client', true );
+    $work_url = get_post_meta( $post->ID, 'work-url', true );
+    $work_date = date('M jS, Y',strtotime(get_post_meta( $post->ID, 'work-date', true )));
+    $terms = get_the_terms( $post->ID , 'works-categories', 'string' );
+    $descpos = get_post_meta( $post->ID , 'work-desc-position', true );
     
-        $permalink = get_permalink( $post->ID );
-        $work_media = unserialize(get_post_meta( $post->ID, 'work-media', true ));
-        $work_client = get_post_meta( $post->ID, 'work-client', true );
-        $work_url = get_post_meta( $post->ID, 'work-url', true );
-        $work_date = date('M jS, Y',strtotime(get_post_meta( $post->ID, 'work-date', true )));
-        $terms = get_the_terms( $post->ID , 'works-categories', 'string' );
-		$descpos = get_post_meta( $post->ID , 'work-desc-position', true );
-		 
-        
-        if ( $terms && ! is_wp_error( $terms ) ) {
+    if ( $terms && ! is_wp_error( $terms ) ) {
         $draught_links = array();
         foreach ( $terms as $term ) {
             $draught_links[] = '<a href="'.esc_attr(get_term_link( $term, 'works-categories' )).'">'.$term->name.'</a>';
         }
         $categories = join( ", ", $draught_links );
-        }
+    }
+    
+
+?>	
         
-    ?>      
-       <div class="postwraps sixteen columns showajaxcontent border-color">
-                      <div class="fifteensp columns offset-by-half alpha">
-                           <h2 class="titles" style="text-shadow:none!important;"><a href="<?php echo $permalink ?>" style="text-shadow:none!important;"><?php the_title(); ?></a></h2>
-                           <hr />
-                      </div>  
-                      <div class="fifteensp columns offset-by-half alpha pinfo">
-                           <div class="four columns alpha">
-                           		<?php if($categories!="") {?>
+<div id="singlecontent">
+        <div class="single-hero works-wrapper">
+            <div class="works-return-wrapper"><a href="#" onclick="history.back()" class="works-return"><span class="fa-solid fa-arrow-left"></span>Retour aux projets</a></div>
+
+            <div class="works-nav">
+                <?php 
+                    // GET CATEGORIES
+                    $tp = 'works-categories';
+                    $cats = get_terms( $tp ); 
+                    $count_cats = count( $cats ); 
+                    if ( $count_cats > 0 ) {
+                ?>
+                    <?php foreach ($cats as $catd) { ?>
+                        <a href="<?php echo esc_attr(get_term_link( $catd, $tp )); ?>" class="works-nav-item <?php if(isset($term) && $catd->slug==$term) { echo 'selected'; } ?>" title="<?php if(isset($term)) echo $term->name; ?>"><?php echo $catd->name; ?></a>
+                    <?php } ?>
+                <?php } ?>
+            </div>
+
+            <h1 class="text-center page-title--primary"><?php the_title(); ?></h1>
+
+            <div class="single-hero-content">
+                      
+                      <?php 
+                          $sharittop = of_get_option('md_social_post_disable_top');
+          
+                          if(!$sharittop) { $coln = 'twelve'; }else{ $coln = 'fifteen'; }
+                                                              
+                          if(!$sharittop) {
+                                $workdesc .=  '<div class="single-hero-desc">';
+                          }
+          
+                          $workdesc .= apply_filters('the_content', $post->post_content);
+                                   
+                          if(!$sharittop) {
+                              $workdesc .= '<div style="display: none;">';
+                                 $workdesc .= '<div>
+                                              <strong>'.__('SHARE','dronetv').'</strong>
+                                              <div class="buttons">';
+                                  
+                              $pimg = getThumb('large');
+                              $ptitle = get_the_title();
+                              $workdesc .= showshareingpost($permalink,@$pimg[0], @$ptitle,false,1); 
+                                          
+                              $workdesc .= '</div></div></div>';
+                          } 
+                                    
+                          if(!$sharittop) {
+                                $workdesc .= '</div>';
+                          }
+                      ?>
+                            
+                      <?php 
+                          /// SHOW PROJECT DESCRIPTION
+                          if($descpos=='top') {
+                              echo $workdesc;  
+                          }
+                      ?>
+
+                <div class="single-hero-attr">
+                    <div>
+                        <div>
+                            <?php if($categories!="") {?>
                                 <strong><?php _e('Creative Fields','dronetv')?></strong> <br />
                                 <?php echo $categories; ?>  
-                                <?php } ?>
-                          </div> 
-
-                           <div class="four columns">
-                           		<?php if($work_url!="") {?>
+                            <?php } ?>
+                        </div>
+                        <div style="margin-top: 15px;">
+                            <?php if($work_url!="") {?>
                                 <strong><?php _e('Agence','dronetv')?></strong> <br />
                                 <a href="<?php echo $work_url; ?>" target="_blank"><?php 
-								$work_url = str_replace('http://','',$work_url);
-								if(strlen($work_url) > 30) {
-								echo substr($work_url,0,30)."..."; 
-								}else{
-								echo $work_url;	
-								}
-								?></a>  
-                          		<?php } ?>
-                          </div>
-			   <div class="four columns">
-                           		<?php if($work_client!="") {?>
+                                $work_url = str_replace('http://','',$work_url);
+                                if(strlen($work_url) > 30) {
+                                    echo substr($work_url,0,30)."..."; 
+                                }else{
+                                    echo $work_url;	
+                                }
+                                ?></a>  
+                            <?php } ?>
+                        </div>
+                    </div>
+                    
+                    <div>
+                    <div>
+                            <?php if($work_client!="") {?>
                                 <strong><?php _e('Client','dronetv')?></strong> <br />
                                 <?php echo $work_client; ?>
-                                <?php } ?>
-                          </div> 
-                           <div class="three columns omega">
-                           		<?php if(get_post_meta( $post->ID, 'work-date', true )) {?>
+                            <?php } ?>
+                        </div> 
+
+                        <div class="three columns omega">
+                            <?php if(get_post_meta( $post->ID, 'work-date', true )) {?>
                                 <strong><?php _e('Completion Date','dronetv')?></strong><br />
                                 <?php echo $work_date; ?> 
-                                <?php } ?>  
-                          </div>
-            			  <br class="clear" />
-                          <hr />
-                      </div>
-                      
-                      <?php 
-					  	  $sharittop = of_get_option('md_social_post_disable_top');
-						 if(!$sharittop) { $coln = 'twelve'; }else{ $coln = 'fifteen'; }
-						  
-						  $workdesc = '<br class="clear" /><div class="fifteensp columns offset-by-half alpha fitvids">';
-						  
-						  if(!$sharittop) {
-						  	$workdesc .=  '<div class="'.$coln.' columns alpha">';
-						  }
-						  $workdesc .= apply_filters('the_content', $post->post_content);
-						  
-						  if(!$sharittop) {
-						  	$workdesc .= '&nbsp;</div>';
-						  }
-						 
-						 if(!$sharittop) {
-							$workdesc .= '<div class="three columns resdontshow omega sharing">';
-				   			$workdesc .= '<div class="sharingbottom border-color tops">
-											<strong>'.__('SHARE','dronetv').'</strong>
-											<br class="clear" />
-											<div class="buttons">';
-							
-							$pimg = getThumb('large');
-							$ptitle = get_the_title();
-							$workdesc .= showshareingpost($permalink,@$pimg[0], @$ptitle,false,1); 
-								
-							$workdesc .= '</div></div></div>';
-						 } 
-                        $workdesc .='</div>';
-					  ?>
-              	
-                      <?php 
-					  	  /// SHOW PROJECT DESCRIPTION
-						  if($descpos=='top') {
-							echo $workdesc;  
-						  }
-					  ?>
-                      
-              <br class="clear" />
-              
+                            <?php } ?>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+       <div class="postwraps sixteen columns showajaxcontent border-color">
               <div class="postcontent fitvids">
               		<?php 
 						$s1=0;
